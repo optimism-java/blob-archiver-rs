17d8afd59f2344b6dae3f4deb6871b13deff5c6d
thinkAfCod <q315xia@163.com>
2024-09-02 17:57:47 +0800

    fix format

--- bin/archiver/src/main.rs
+++ bin/archiver/src/main.rs
@@ -177,7 +177,11 @@ pub struct CliArgs {
     #[clap(long, env = "LOG_DIR")]
     log_dir: Option<String>,
 
-    #[clap(long, env = "LOG_ROTATION", help = "Log rotation values: DAILY, HOURLY, MINUTELY, NEVER")]
+    #[clap(
+        long,
+        env = "LOG_ROTATION",
+        help = "Log rotation values: DAILY, HOURLY, MINUTELY, NEVER"
+    )]
     log_rotation: Option<String>,
 
     #[clap(long, env = "BEACON_ENDPOINT", required = true)]
